f5646e6d1acc0daddaf633fe3afb8776ce65666e
Maxence Ho <0ho.maxence0@gmail.com>
2018-09-07 23:52:50 +0200

    Initial commit

---
+++ MVILogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVILogin
+//
+//  Created by maxence on 07/09/2018.
+//  Copyright © 2018 maxence_ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
